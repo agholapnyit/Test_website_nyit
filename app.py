48825986e015e2b08aaef66886ed924998d2fd78
agholapnyit <144952816+agholapnyit@users.noreply.github.com>
2023-09-19 08:58:25 +0000

    flask 2

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+import flask
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_wrold():
-  return "HELLO CLIENT THIS IS IRM CONSULTING"
+def hello_world():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug= False)
+  app.run(host='0.0.0.0', debug=False)
